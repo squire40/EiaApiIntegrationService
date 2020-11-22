33fba0eaefa0595095add4aa117722aa31e61cc9
Dave Bourke <squire40@hotmail.com>
2020-11-22 16:44:21 -0500

    Added Startup.cs

---
+++ EiaApiIntegrationService/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace EiaApiIntegrationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceCollection services = new ServiceCollection();
+            Startup startup = new Startup();
+            startup.ConfigureServices(services);
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+
+        }
+    }
+}
---
+++ EiaApiIntegrationService/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Startup
+{
+    IConfigurationRoot Configuration { get; }
+
+    public Startup()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json");
+
+        Configuration = builder.Build();
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<IConfigurationRoot>(Configuration);
+        //services.AddSingleton<IMyService, MyService>();
+    }
+}
